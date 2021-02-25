fce191750cff37f34183669a40f9cc4e08ce8ea3
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-02-24 21:31:42 -0800

    cleanliness

--- Okta.Xamarin/Okta.Xamarin/RenewEventArgs.cs
+++ Okta.Xamarin/Okta.Xamarin/RenewEventArgs.cs
@@ -1,16 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="RenewEventArgs.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
 
 namespace Okta.Xamarin
 {
-	public class RenewEventArgs : EventArgs
-	{
-		public IOktaStateManager StateManager{ get; set; }
+    /// <summary>
+    /// Represents data relevant to renew events.
+    /// </summary>
+    public class RenewEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets or sets the state manager.
+        /// </summary>
+        public IOktaStateManager StateManager{ get; set; }
 
-		public bool RefreshIdToken { get; set; }
-		public string AuthorizationServerId { get; set; }
+        /// <summary>
+        /// Gets or sets a value indication whether the id token is refreshed.
+        /// </summary>
+        public bool RefreshIdToken { get; set; }
 
-		public RenewResponse Response { get; set; }
-	}
+        /// <summary>
+        /// Gets or sets the authorization server id.
+        /// </summary>
+        public string AuthorizationServerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the response.
+        /// </summary>
+        public RenewResponse Response { get; set; }
+    }
 }
--- Okta.Xamarin/Okta.Xamarin/RenewOptions.cs
+++ Okta.Xamarin/Okta.Xamarin/RenewOptions.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="RenewOptions.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
 
 namespace Okta.Xamarin
 {
-	public class RenewOptions
-	{
-		public RenewOptions()
-		{
-			AuthorizationServerId = "default";
-		}
+    public class RenewOptions
+    {
+        public RenewOptions()
+        {
+            AuthorizationServerId = "default";
+        }
 
-		public string RefreshToken { get; set; }
-		public bool RefreshIdToken { get; set; }
+        public string RefreshToken { get; set; }
 
-		public string AuthorizationServerId { get; set; }
-	}
+        public bool RefreshIdToken { get; set; }
+
+        public string AuthorizationServerId { get; set; }
+    }
 }
