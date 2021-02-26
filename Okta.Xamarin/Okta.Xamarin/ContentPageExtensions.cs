75a3b4708012272b8244da2109a78efeb4feb387
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-02-26 08:14:27 -0800

    header and doc comments

--- Okta.Xamarin/Okta.Xamarin/ContentPageExtensions.cs
+++ Okta.Xamarin/Okta.Xamarin/ContentPageExtensions.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿// <copyright file="ContentPageExtensions.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
 using System.Collections.Generic;
-using System.Text;
 using Xamarin.Forms;
 
 namespace Okta.Xamarin
 {
+    /// <summary>
+    /// Extension metods for ContentPage.
+    /// </summary>
     public static class ContentPageExtensions
     {
+        /// <summary>
+        /// Adds the specified data to the stack layout with the specified name.
+        /// </summary>
+        /// <param name="contentPage">The content page</param>
+        /// <param name="data">The data</param>
+        /// <param name="stackLayoutElementName">The name of the stack layout.</param>
         public static void DisplayData(this ContentPage contentPage, Dictionary<string, object> data, string stackLayoutElementName)
         {
             StackLayout claimsLayout = (StackLayout)contentPage.FindByName(stackLayoutElementName);
