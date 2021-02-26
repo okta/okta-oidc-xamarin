f734c585c9397a39e6bda454d6fb1a0079ea5072
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-02-26 09:03:26 -0800

    doc comments

--- Okta.Xamarin/Okta.Xamarin/OktaStateManager.cs
+++ Okta.Xamarin/Okta.Xamarin/OktaStateManager.cs
@@ -3,10 +3,8 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 // </copyright>
 
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -28,12 +26,12 @@ namespace Okta.Xamarin
         /// <summary>
         /// Initializes a new instance of the <see cref="OktaStateManager"/> class.
         /// </summary>
-        /// <param name="accessToken"></param>
-        /// <param name="tokenType"></param>
-        /// <param name="idToken"></param>
-        /// <param name="refreshToken"></param>
-        /// <param name="expiresIn"></param>
-        /// <param name="scope"></param>
+        /// <param name="accessToken">The access token.</param>
+        /// <param name="tokenType">The token type.</param>
+        /// <param name="idToken">The ID token.</param>
+        /// <param name="refreshToken">The refresh token.</param>
+        /// <param name="expiresIn">Expires in.</param>
+        /// <param name="scope">Scope.</param>
         public OktaStateManager(string accessToken, string tokenType, string idToken = null, string refreshToken = null, int? expiresIn = null, string scope = null)
         {
             this.TokenType = tokenType;
@@ -44,45 +42,36 @@ namespace Okta.Xamarin
             this.Scope = scope;
         }
 
-        /// <summary>
-        /// Gets the type of the AccessToken, for example "Bearer".
-        /// </summary>
+        /// <inheritdoc/>
         public string TokenType { get; private set; }
 
-        /// <summary>
-        /// Gets the access token.
-        /// </summary>
+        /// <inheritdoc/>
         public string AccessToken { get; private set; }
 
-        /// <summary>
-        /// Gets the id token.
-        /// </summary>
+        /// <inheritdoc/>
         public string IdToken { get; private set; }
 
-        /// <summary>
-        /// Gets the refresh token.
-        /// </summary>
+        /// <inheritdoc/>
         public string RefreshToken { get; private set; }
 
-        /// <summary>
-        /// Gets the scope.
-        /// </summary>
+        /// <inheritdoc/>
         public string Scope { get; private set; }
 
-        /// <summary>
-        /// Gets the expiration time.
-        /// </summary>
+        /// <inheritdoc/>
         public DateTime? Expires { get; private set; }
 
+        /// <summary>
+        /// Gets the most recent renew response or null.
+        /// </summary>
         public RenewResponse RenewResponse { get; private set; }
 
+        /// <inheritdoc/>
         public IOktaConfig Config { get; set; }
 
+        /// <inheritdoc/>
         public IOidcClient Client { get; set; }
 
-        /// <summary>
-        /// Gets a value indicating whether or not there is a current non-expired <see cref="AccessToken"/>, indicating the user is currently successfully authenticated
-        /// </summary>
+        /// <inheritdoc/>
         public bool IsAuthenticated
         {
             get
@@ -92,34 +81,13 @@ namespace Okta.Xamarin
             }
         }
 
-        /// <summary>
-        /// Gets or sets the last response received from the API. Primarily for debugging.
-        /// </summary>
+        /// <inheritdoc/>
         public HttpResponseMessage LastApiResponse { get => Client?.LastApiResponse; }
 
-        public string GetIdToken()
+        /// <inheritdoc/>
+        public string GetToken(TokenKind tokenKind)
         {
-            return GetToken(Xamarin.TokenKind.IdToken);
-        }
-
-        public string GetAccessToken()
-        {
-            return GetToken(Xamarin.TokenKind.AccessToken);
-        }
-
-        public string GetRefreshToken()
-        {
-            return GetToken(Xamarin.TokenKind.RefreshToken);
-        }
-
-        /// <summary>
-        /// Gets the token of the specified type.
-        /// </summary>
-        /// <param name="tokenType">The token type.</param>
-        /// <returns>string</returns>
-        public string GetToken(TokenKind tokenType)
-        {
-            switch (tokenType)
+            switch (tokenKind)
             {
                 case Xamarin.TokenKind.Invalid:
                 case Xamarin.TokenKind.AccessToken:
@@ -132,29 +100,19 @@ namespace Okta.Xamarin
             }
         }
 
-        /// <summary>
-        /// Stores the tokens securely in platform-specific secure storage.  This is an async method and should be awaited.
-        /// </summary>
-        /// <returns>Task which tracks the progress of the save.</returns>
+        /// <inheritdoc/>
         public async Task WriteToSecureStorageAsync() // to be implemented on OKTA-363613 v1.1
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Retrieves a stored state manager for a given config.  This is an async method and should be awaited.
-        /// </summary>
-        /// <param name="config">the Okta configuration that corresponds to a manager you are interested in</param>
-        /// <returns>If a state manager is found for the provided config, this Task will return the <see cref="OktaStateManager"/>.</returns>
-        public static async Task<OktaStateManager> ReadFromSecureStorageAsync(IOktaConfig config) // to be implemented on OKTA-363613 v1.1
+        /// <inheritdoc/>
+        public async Task<OktaStateManager> ReadFromSecureStorageAsync(IOktaConfig config) // to be implemented on OKTA-363613 v1.1
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Revokes tokens associated with this OktaState.
-        /// </summary>
-        /// <returns>Task.</returns>
+        /// <inheritdoc/>
         public virtual async Task RevokeAsync(TokenKind tokenType)
         {
             switch (tokenType)
@@ -169,33 +127,19 @@ namespace Okta.Xamarin
             }
         }
 
-        /// <summary>
-        /// Gets an instance of the generic type T representing the current user.
-        /// </summary>
-        /// <typeparam name="T">The type to deserialize the response as.</typeparam>
-        /// <param name="authorizationServerId">The authorization server ID.</param>
-        /// <returns>T.</returns>
+        /// <inheritdoc/>
         public async Task<T> GetUserAsync<T>(string authorizationServerId = "default")
         {
             return await Client.GetUserAsync<T>(AccessToken, authorizationServerId);
         }
 
-        /// <summary>
-        /// Gets information about the current user.
-        /// </summary>
-        /// <param name="authorizationServerId">The authorization server ID.</param>
-        /// <returns>Dictionary{string, object}.</returns>
+        /// <inheritdoc/>
         public async Task<Dictionary<string, object>> GetUserAsync(string authorizationServerId = "default")
         {
             return await Client.GetUserAsync(AccessToken, authorizationServerId);
         }
 
-        /// <summary>
-        /// Gets information about the state of the specified token.
-        /// </summary>
-        /// <param name="tokenKind">The kind of token.</param>
-        /// <param name="authorizationServerId">The authorization server ID.</param>
-        /// <returns>Dictoinary{string, object}.</returns>
+        /// <inheritdoc/>
         public async Task<Dictionary<string, object>> IntrospectAsync(TokenKind tokenKind, string authorizationServerId = "default")
         {
             return await Client.IntrospectAsync(new IntrospectOptions
@@ -206,21 +150,13 @@ namespace Okta.Xamarin
             });
         }
 
-        /// <summary>
-        /// Calls the OpenID Connect UserInfo endpoint with the stored access token to return user claim information.  This is an async method and should be awaited.
-        /// </summary>
-        /// <returns>A Task with a<see cref="System.Security.Claims.ClaimsPrincipal"/> representing the current user.</returns>
+        /// <inheritdoc/>
         public async Task<ClaimsPrincipal> GetClaimsPrincipalAsync(string authorizationServerId = "default")
         {
             return await Client.GetClaimsPincipalAsync(AccessToken, authorizationServerId);
         }
 
-        /// <summary>
-        /// Renews tokens.
-        /// </summary>
-        /// <param name="refreshIdToken">A value indicating whether to also renew the ID token.</param>
-        /// <param name="authorizationServerId">The authorization server ID.</param>
-        /// <returns></returns>
+        /// <inheritdoc/>
         public async Task<RenewResponse> RenewAsync(bool refreshIdToken = false, string authorizationServerId = "default")
         {
             RenewResponse renewResponse = await Client.RenewAsync<RenewResponse>(RefreshToken, refreshIdToken, authorizationServerId);
@@ -236,9 +172,7 @@ namespace Okta.Xamarin
             return renewResponse;
         }
 
-        /// <summary>
-        /// Removes the local authentication state. A full sign out should consist of calling <see cref="OidcClient.SignOutOfOktaAsync"/>, then <see cref="RevokeAsync"/>, and then <see cref="Clear"/>.
-        /// </summary>
+        /// <inheritdoc/>
         public void Clear()
         {
             AccessToken = string.Empty;
