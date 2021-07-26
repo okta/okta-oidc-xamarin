97508868efdf67b7fad6edc2b4275185e3b10444
Bryan Apellanes <63638027+bryanapellanes-okta@users.noreply.github.com>
2021-07-26 05:44:51 -0700

    OKTA-373355: add TinyIoCContainer and related tests (#52)

---
+++ Okta.Xamarin/Tests/Okta.Xamarin.Test/TinyIoCContainerShould.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="TinyIoCContainerShould.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using TinyIoC;
+using Xunit;
+
+namespace Okta.Xamarin.Test
+{
+    public class TinyIocContainerShould
+    {
+        [Fact]
+        public async Task GetInstance()
+        {
+            TinyIoCContainer container = new TinyIoCContainer();
+            OktaContext context = container.Resolve<OktaContext>();
+            context.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task RegisterInterfaceMapping()
+        {
+            TinyIoCContainer container = new TinyIoCContainer();
+            container.Register<IOktaStateManager, TestOktaStateManager>();
+
+            IOktaStateManager oktaStateManager = container.Resolve<IOktaStateManager>();
+
+            oktaStateManager.Should().NotBeOfType(typeof(OktaStateManager));
+            oktaStateManager.Should().BeOfType(typeof(TestOktaStateManager));
+        }
+    }
+}
