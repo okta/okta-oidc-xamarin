a7e11f2c8f7709caf28a0ad3087d0b2415579dd0
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-08-04 08:07:35 -0700

    moar docs

--- Okta.Xamarin/Okta.Xamarin/InitServicesEventArgs.cs
+++ Okta.Xamarin/Okta.Xamarin/InitServicesEventArgs.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="InitServicesEventArgs.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
 using Okta.Xamarin.TinyIoC;
 
 namespace Okta.Xamarin
 {
+    /// <summary>
+    /// Represents arguments relevant to initializing services events.
+    /// </summary>
     public class InitServicesEventArgs : EventArgs
     {
+        /// <summary>
+        /// Gets or sets the inversion of control container.
+        /// </summary>
         public TinyIoCContainer TinyIoCContainer { get; set; }
 
+        /// <summary>
+        /// Gets or sets the exception if any, may be null.
+        /// </summary>
         public Exception Exception { get; set; }
     }
 }
--- Okta.Xamarin/Okta.Xamarin/OktaStateManager.cs
+++ Okta.Xamarin/Okta.Xamarin/OktaStateManager.cs
@@ -251,11 +251,19 @@ namespace Okta.Xamarin
             return this;
         }
 
+        /// <summary>
+        /// Raises the SecureStorageReadStarted event.
+        /// </summary>
+        /// <param name="eventArgs"></param>
         protected void OnSecureStorageReadStarted(SecureStorageEventArgs eventArgs)
         {
             this.SecureStorageReadStarted?.Invoke(this, eventArgs);
         }
 
+        /// <summary>
+        /// Raises the SecureStorageReadCompleted event.
+        /// </summary>
+        /// <param name="eventArgs"></param>
         protected void OnSecureStorageReadCompleted(SecureStorageEventArgs eventArgs)
         {
             this.SecureStorageReadCompleted?.Invoke(this, eventArgs);
