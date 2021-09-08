e3934b3b13737142e144f191365466baf0f0fee7
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-09-08 08:33:13 -0700

    fix formatting and docs

--- Okta.Xamarin/Okta.Xamarin/Services/OktaLogger.cs
+++ Okta.Xamarin/Okta.Xamarin/Services/OktaLogger.cs
@@ -10,16 +10,16 @@ using Newtonsoft.Json;
 
 namespace Okta.Xamarin.Services
 {
-	/// <summary>
-	/// The default Okta logger implementation.
-	/// </summary>
+    /// <summary>
+    /// The default Okta logger implementation.
+    /// </summary>
     public class OktaLogger : IOktaLogger
     {
-		/// <summary>
-		/// Initializes a new instance of the <see cref="OktaLogger"/> class.
-		/// </summary>
-		/// <param name="logger">The base logger.</param>
-		public OktaLogger(ILogger logger)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OktaLogger"/> class.
+        /// </summary>
+        /// <param name="logger">The base logger.</param>
+        public OktaLogger(ILogger logger)
         {
             if (logger == null)
             {
@@ -73,119 +73,120 @@ namespace Okta.Xamarin.Services
             };
         }
 
-		/// <summary>
-		/// Gets the logger.
-		/// </summary>
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
         protected ILogger Logger { get; }
 
-		/// <summary>
-		/// Gets or sets the default information event handler.
-		/// </summary>
+        /// <summary>
+        /// Gets or sets the default information event handler.
+        /// </summary>
         protected EventHandler DefaultInfoEventhandler { get; set; }
 
-		/// <summary>
-		/// Gets or sets the default warning event handler.
-		/// </summary>
+        /// <summary>
+        /// Gets or sets the default warning event handler.
+        /// </summary>
         protected EventHandler DefaultWarningEventHandler { get; set; }
 
-		/// <summary>
-		/// Gets or sets the default error event handler.
-		/// </summary>
+        /// <summary>
+        /// Gets or sets the default error event handler.
+        /// </summary>
         protected EventHandler DefaultErrorEventHandler { get; set; }
 
-		/// <summary>
-		/// Gets or sets the default fatal event handler.
-		/// </summary>
+        /// <summary>
+        /// Gets or sets the default fatal event handler.
+        /// </summary>
         protected EventHandler DefaultFatalEventHandler { get; set; }
 
-		/// <summary>
-		/// Log the specified error message.
-		/// </summary>
-		/// <param name="messageFormat">The string compatible message format.</param>
-		/// <param name="args">The message arguments.</param>
+        /// <summary>
+        /// Log the specified error message.
+        /// </summary>
+        /// <param name="messageFormat">The string compatible message format.</param>
+        /// <param name="args">The message arguments.</param>
         public virtual void Error(string messageFormat, params object[] args)
         {
             this.Logger.Error(messageFormat, args);
         }
 
-		/// <summary>
-		/// Log the specified fatal message.
-		/// </summary>
-		/// <param name="messageFormat">The string compatible message format.</param>
-		/// <param name="args">The message arguments.</param>
-		public virtual void Fatal(string messageFormat, params object[] args)
+        /// <summary>
+        /// Log the specified fatal message.
+        /// </summary>
+        /// <param name="messageFormat">The string compatible message format.</param>
+        /// <param name="args">The message arguments.</param>
+        public virtual void Fatal(string messageFormat, params object[] args)
         {
             this.Logger.Fatal(messageFormat, args);
         }
 
-		/// <summary>
-		/// Log the specified information message.
-		/// </summary>
-		/// <param name="messageFormat">The string compatible message format.</param>
-		/// <param name="args">The message arguments.</param>
-		public virtual void Info(string messageFormat, params object[] args)
+        /// <summary>
+        /// Log the specified information message.
+        /// </summary>
+        /// <param name="messageFormat">The string compatible message format.</param>
+        /// <param name="args">The message arguments.</param>
+        public virtual void Info(string messageFormat, params object[] args)
         {
             this.Logger.Info(messageFormat, args);
         }
 
-		/// <summary>
-		/// Log the specified warning message.
-		/// </summary>
-		/// <param name="messageFormat">The string compatible message format.</param>
-		/// <param name="args">The message arguments.</param>
-		public virtual void Warning(string messageFormat, params object[] args)
+        /// <summary>
+        /// Log the specified warning message.
+        /// </summary>
+        /// <param name="messageFormat">The string compatible message format.</param>
+        /// <param name="args">The message arguments.</param>
+        public virtual void Warning(string messageFormat, params object[] args)
         {
             this.Logger.Warning(messageFormat, args);
         }
 
-		/// <summary>
-		/// Log all relevant events using the default logging behavior.
-		/// </summary>
-		public void LogAllEvents()
-		{
-			LogLoadStateStartedEvents();
-			LogLoadStateCompletedEvents();
-			LogLoadStateExceptionEvents();
-			LogSecureStorageWriteStartedEvents();
-			LogSecureStorageWriteCompletedEvents();
-			LogSecureStorageWriteExceptionEvents();
-			LogSecureStorageReadStartedEvents();
-			LogSecureStorageReadCompletedEvents();
-			LogSecureStorageReadExceptionEvents();
-			LogRequestExceptionEvents();
-			LogSignInStartedEvents();
-			LogSignInCompletedEvents();
-			LogSignOutStartedEvents();
-			LogSignOutCompletedEvents();
-			LogAuthenticationFailedEvents();
-			LogRevokeStartedEvents();
-			LogRevokeCompletedEvents();
-			LogRevokeExceptionEvents();
-			LogGetUserStartedEvents();
-			LogGetUserCompletedEvents();
-			LogIntrospectStartedEvents();
-			LogIntrospectCompletedEvents();
-			LogRenewStartedEvents();
-			LogRenewCompletedEvents();
-			LogRenewExceptionEvents();
-		}
+        /// <summary>
+        /// Log all relevant events using the default logging behavior.
+        /// </summary>
+        public void LogAllEvents()
+        {
+            this.LogLoadStateStartedEvents();
+            this.LogLoadStateCompletedEvents();
+            this.LogLoadStateExceptionEvents();
+            this.LogSecureStorageWriteStartedEvents();
+            this.LogSecureStorageWriteCompletedEvents();
+            this.LogSecureStorageWriteExceptionEvents();
+            this.LogSecureStorageReadStartedEvents();
+            this.LogSecureStorageReadCompletedEvents();
+            this.LogSecureStorageReadExceptionEvents();
+            this.LogRequestExceptionEvents();
+            this.LogSignInStartedEvents();
+            this.LogSignInCompletedEvents();
+            this.LogSignOutStartedEvents();
+            this.LogSignOutCompletedEvents();
+            this.LogAuthenticationFailedEvents();
+            this.LogRevokeStartedEvents();
+            this.LogRevokeCompletedEvents();
+            this.LogRevokeExceptionEvents();
+            this.LogGetUserStartedEvents();
+            this.LogGetUserCompletedEvents();
+            this.LogIntrospectStartedEvents();
+            this.LogIntrospectCompletedEvents();
+            this.LogRenewStartedEvents();
+            this.LogRenewCompletedEvents();
+            this.LogRenewExceptionEvents();
+        }
 
-		/// <inheritdoc/>
-		public void LogRequestExceptionEvents(EventHandler<RequestExceptionEventArgs> eventHandler = null)
-		{
-			OktaContext.Current.RequestException += (sender, args) =>
-			{
-				if (eventHandler == null)
-				{
-					this.DefaultErrorEventHandler(sender, args);
-				}
-				else
-				{
-					eventHandler(sender, args);
-				}
-			};
-		}
+        /// <inheritdoc/>
+        public void LogRequestExceptionEvents(EventHandler<RequestExceptionEventArgs> eventHandler = null)
+        {
+            OktaContext.Current.RequestException += (sender, args) =>
+            {
+                if (eventHandler == null)
+                {
+                    this.DefaultErrorEventHandler(sender, args);
+                }
+                else
+                {
+                    eventHandler(sender, args);
+                }
+            };
+        }
 
+        /// <inheritdoc/>
         public void LogAuthenticationFailedEvents(EventHandler<AuthenticationFailedEventArgs> eventHandler = null)
         {
             OktaContext.AddAuthenticationFailedListener((sender, args) =>
@@ -201,6 +202,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogGetUserCompletedEvents(EventHandler<GetUserEventArgs> eventHandler = null)
         {
             OktaContext.AddGetUserCompletedListener((sender, args) =>
@@ -216,6 +218,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogGetUserStartedEvents(EventHandler<GetUserEventArgs> eventHandler = null)
         {
             OktaContext.AddGetUserStartedListener((sender, args) =>
@@ -231,6 +234,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogIntrospectCompletedEvents(EventHandler<IntrospectEventArgs> eventHandler = null)
         {
             OktaContext.AddIntrospectCompletedListener((sender, args) =>
@@ -246,6 +250,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogIntrospectStartedEvents(EventHandler<IntrospectEventArgs> eventHandler = null)
         {
             OktaContext.AddIntrospectStartedListener((sender, args) =>
@@ -261,6 +266,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogLoadStateCompletedEvents(EventHandler<SecureStorageEventArgs> eventHandler = null)
         {
             OktaContext.AddLoadStateCompletedListener((sender, args) =>
@@ -276,6 +282,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogLoadStateExceptionEvents(EventHandler<SecureStorageExceptionEventArgs> eventHandler = null)
         {
             OktaContext.AddLoadStateExceptionListener((sender, args) =>
@@ -291,6 +298,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogLoadStateStartedEvents(EventHandler<SecureStorageEventArgs> eventHandler = null)
         {
             OktaContext.AddLoadStateStartedListener((sender, args) =>
@@ -306,6 +314,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogRenewCompletedEvents(EventHandler<RenewEventArgs> eventHandler = null)
         {
             OktaContext.AddRenewCompletedListener((sender, args) =>
@@ -321,6 +330,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogRenewExceptionEvents(EventHandler<RenewExceptionEventArgs> eventHandler = null)
         {
             OktaContext.AddRenewExceptionListener((sender, args)
@@ -337,6 +347,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogRenewStartedEvents(EventHandler<RenewEventArgs> eventHandler = null)
         {
             OktaContext.AddRenewStartedListener((sender, args) =>
@@ -352,6 +363,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogRevokeCompletedEvents(EventHandler<RevokeEventArgs> eventHandler = null)
         {
             OktaContext.AddRevokeCompletedListener((sender, args) =>
@@ -367,6 +379,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogRevokeExceptionEvents(EventHandler<RevokeExceptionEventArgs> eventHandler = null)
         {
             OktaContext.AddRevokeExceptionListener((sender, args) =>
@@ -382,6 +395,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogRevokeStartedEvents(EventHandler<RevokeEventArgs> eventHandler = null)
         {
             OktaContext.AddRevokeStartedListener((sender, args) =>
@@ -397,6 +411,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSecureStorageReadCompletedEvents(EventHandler<SecureStorageEventArgs> eventHandler = null)
         {
             OktaContext.AddSecureStorageReadCompletedListener((sender, args) =>
@@ -412,6 +427,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSecureStorageReadExceptionEvents(EventHandler<SecureStorageExceptionEventArgs> eventHandler = null)
         {
             OktaContext.AddSecureStorageReadExceptionListener((sender, args) =>
@@ -427,6 +443,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSecureStorageReadStartedEvents(EventHandler<SecureStorageEventArgs> eventHandler = null)
         {
             OktaContext.AddSecureStorageReadStartedListener((sender, args) =>
@@ -442,6 +459,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSecureStorageWriteCompletedEvents(EventHandler<SecureStorageEventArgs> eventHandler = null)
         {
             OktaContext.AddSecureStorageWriteCompletedListener((sender, args) =>
@@ -457,6 +475,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSecureStorageWriteExceptionEvents(EventHandler<SecureStorageExceptionEventArgs> eventHandler = null)
         {
             OktaContext.AddSecureStorageWriteExceptionListener((sender, args) =>
@@ -472,6 +491,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSecureStorageWriteStartedEvents(EventHandler<SecureStorageEventArgs> eventHandler = null)
         {
             OktaContext.AddSecureStorageWriteStartedListener((sender, args) =>
@@ -487,6 +507,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSignInCompletedEvents(EventHandler<SignInEventArgs> eventHandler = null)
         {
             OktaContext.AddSignInCompletedListener((sender, args) =>
@@ -502,6 +523,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSignInStartedEvents(EventHandler<SignInEventArgs> eventHandler = null)
         {
             OktaContext.AddSignInStartedListener((sender, args) =>
@@ -517,6 +539,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSignOutCompletedEvents(EventHandler<SignOutEventArgs> eventHandler = null)
         {
             OktaContext.AddSignOutCompletedListener((sender, args) =>
@@ -532,6 +555,7 @@ namespace Okta.Xamarin.Services
             });
         }
 
+        /// <inheritdoc/>
         public void LogSignOutStartedEvents(EventHandler<SignOutEventArgs> eventHandler = null)
         {
             OktaContext.AddSignOutStartedListener((sender, args) =>
