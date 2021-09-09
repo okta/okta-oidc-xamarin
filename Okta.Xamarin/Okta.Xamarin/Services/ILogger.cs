9dab3ced8b4500434e6bec5b2740bf943256a4b3
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-09-09 08:03:28 -0700

    add headers, clean usings

--- Okta.Xamarin/Okta.Xamarin/Services/ILogger.cs
+++ Okta.Xamarin/Okta.Xamarin/Services/ILogger.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="ILogger.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
 
 namespace Okta.Xamarin.Services
 {
-    /// <summary>
-    /// An interface defining logging methods.
-    /// </summary>
-    public interface ILogger
+	/// <summary>
+	/// An interface defining logging methods.
+	/// </summary>
+	public interface ILogger
     {
         /// <summary>
         /// Log an information message.
--- Okta.Xamarin/Okta.Xamarin/Services/IOktaLogger.cs
+++ Okta.Xamarin/Okta.Xamarin/Services/IOktaLogger.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="IOktaLogger.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
 
 namespace Okta.Xamarin.Services
 {
-    /// <summary>
-    /// An interface defining Okta specific event subscription handling methods.
-    /// </summary>
-    public interface IOktaLogger : ILogger
+	/// <summary>
+	/// An interface defining Okta specific event subscription handling methods.
+	/// </summary>
+	public interface IOktaLogger : ILogger
     {
         /// <summary>
         /// Subscribes to and logs all relevant events.
