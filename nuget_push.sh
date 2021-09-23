0732b31e315c095d5ba6086effe30df02799ec4e
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-09-23 07:19:52 -0700

    delete unused build scripts in favor of github actions

--- nuget_push.sh
+++ nuget_push.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# if run from azure, this script pushes nuget packages to github packages
-# if run from bacon, this script pushes nuget packages to artifactory
-
-source ./configure.sh
-GITHUB_PACKAGE_SOURCE="https://nuget.pkg.github.com/okta/index.json"
-
-if [ ${BUILD_ENVIRONMENT} == "AZURE" ]; then
-    echo "adding github package source: ${GITHUB_PACKAGE_SOURCE}"
-    dotnet nuget add source ${GITHUB_PACKAGE_SOURCE} -n github -u ${GITHUB_USERNAME} -p ${GITHUB_ACCESS_TOKEN} --store-password-in-clear-text
-    echo "setting nuget source to github"
-    NUGET_SOURCE="github"
-    NUGET_API_KEY=${GITHUB_ACCESS_TOKEN}
-else
-    # nuget source OKTA_NUGET_TOPIC_REPO and api key ${ARTIFACTORY_NUGET_APIKEY} are set by eng-productivity scripts run in bacon
-    echo "setting nuget source to artifactory"
-    NUGET_SOURCE="OKTA_NUGET_TOPIC_REPO"
-    NUGET_API_KEY=${ARTIFACTORY_NUGET_APIKEY}
-fi
+# nuget source OKTA_NUGET_TOPIC_REPO and api key ${ARTIFACTORY_NUGET_APIKEY} are set by eng-productivity scripts run in bacon
+echo "setting nuget source to artifactory"
+NUGET_SOURCE="OKTA_NUGET_TOPIC_REPO"
+NUGET_API_KEY=${ARTIFACTORY_NUGET_APIKEY}
 
 if [ -d "./nuget/packages" ]; then
     for NUGETPACKAGE in $(ls ./nuget/packages/*.nupkg)
@@ -28,17 +14,4 @@ if [ -d "./nuget/packages" ]; then
     done
 else
     echo "./nuget/packages: No nuget packages found";
-fi
-
-if [[ -f ./release ]]; then
-    echo 'Removing release file'
-    rm ./release
-    if [ ${BUILD_ENVIRONMENT} == "BACON" ]; then
-        VERSION=`cat ./nuget/semver/version`
-        echo "Creating release-${VERSION} branch"
-        git checkout -b release-v${VERSION}
-        git commit --author="bacon-ci <noreply@okta.com>" -am 'BACON: removed release file'
-        git push -u origin release-v${VERSION}
-        echo "Create a pull request for release-${VERSION} to merge to master."                
-    fi
 fi
