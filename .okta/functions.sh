9a117db02f0c18c0cb0a5a3a1fc29e4b1ef627ad
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-10-04 07:18:47 -0700

    move nuget variables

--- .okta/functions.sh
+++ .okta/functions.sh
@@ -77,6 +77,11 @@ function downloadNugetArtifacts() {
 }
 
 function pushNugetsToArtifactory() {
+    # nuget source OKTA_NUGET_TOPIC_REPO and api key ${ARTIFACTORY_NUGET_APIKEY} are set by eng-productivity scripts run in bacon
+    echo "setting nuget source to artifactory"
+    NUGET_SOURCE="OKTA_NUGET_TOPIC_REPO"
+    NUGET_API_KEY=${ARTIFACTORY_NUGET_APIKEY}
+
     if [ -d "./nuget/packages" ]; then
     for NUGETPACKAGE in $(ls ./nuget/packages/*.nupkg)
         do
--- .okta/nuget_push.sh
+++ .okta/nuget_push.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 source ./.okta/functions.sh
-# nuget source OKTA_NUGET_TOPIC_REPO and api key ${ARTIFACTORY_NUGET_APIKEY} are set by eng-productivity scripts run in bacon
-echo "setting nuget source to artifactory"
-NUGET_SOURCE="OKTA_NUGET_TOPIC_REPO"
-NUGET_API_KEY=${ARTIFACTORY_NUGET_APIKEY}
 
 configureEnvironment
 
