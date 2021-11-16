280fbd92fe48ac0a67839dbdce5b66580be54955
Bryan Apellanes <bryan.apellanes@okta.com>
2021-11-16 07:24:50 -0800

    fix indentation

--- Okta.Xamarin/Okta.Xamarin/DefaultOidcClient.cs
+++ Okta.Xamarin/Okta.Xamarin/DefaultOidcClient.cs
@@ -10,18 +10,18 @@ namespace Okta.Xamarin
 {
     public class DefaultOidcClient : OidcClient
     {
-		private static Lazy<string> userAgent = new Lazy<string>(() => $"Okta-Xamarin-Sdk/{Assembly.GetExecutingAssembly().GetName().Version}");
+        private static Lazy<string> userAgent = new Lazy<string>(() => $"Okta-Xamarin-Sdk/{Assembly.GetExecutingAssembly().GetName().Version}");
 
-		protected override void CloseBrowser()
+        protected override void CloseBrowser()
         {
         }
 
-		protected override string GetUserAgent()
-		{
-			return userAgent.Value;
-		}
+        protected override string GetUserAgent()
+        {
+            return userAgent.Value;
+        }
 
-		protected override void LaunchBrowser(string url)
+        protected override void LaunchBrowser(string url)
         {
         }
     }
